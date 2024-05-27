0bffb379bcb9c226b57d270a9184bc3cb9bd695c
Harishviswa1 <136291189+Harishviswa1@users.noreply.github.com>
2024-05-27 21:23:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-      await mongoose.connect('mongodb+srv://harishviswanathk2022it:harish@cluster0.abe6amd.mongodb.net/food');
+      await mongoose.connect('mongodb+srv://#####@cluster0.abe6amd.mongodb.net/food');
       console.log("DB Connected");
     } catch (error) {
       console.error("Error connecting to database:", error);
